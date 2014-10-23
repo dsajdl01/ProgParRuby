e8631cc3d87b7b6fb2640196986034a42e11e792
dsajdl01 <davidsajdl@yahoo.co.uk>
2014-10-23 15:52:14 +0100

    adding input and output for person class

--- to_file.rb
+++ to_file.rb
@@ -19,3 +19,23 @@ class Person
 		name
 	end
 end
+
+# load to_file.rb 
+irb(main):018:0> load './ruby/to_file.rb'
+=> true
+
+#input
+irb(main):019:0> Person.new("David").to_f
+#output
+=> 5
+# it also creates .txt file name similar as object_21446740.txt with content David.
+
+#input
+irb(main):021:0> Person.new("Rita").to_s
+#output
+=> "Rita"
+
+#input
+rb(main):021:0> Person.new("Rita and David").to_f
+#output
+=> 14
