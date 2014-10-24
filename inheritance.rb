8e7db74c17ce832d59ce57a47fd2f609bdd07085
dsajdl01 <davidsajdl@yahoo.co.uk>
2014-10-24 20:44:34 +0100

    adding extra class and modify some methods

--- inheritance.rb
+++ inheritance.rb
@@ -1,33 +1,79 @@
 class A
 	attr_accessor :number
 	
-	def initialize()
-		@number = 10
+	def initialize(number)
+		self.setValue(number)
 	end
 
-	def get_value()
-		return @number
+	def getValue()
+		return @number.to_s + " A Class"
 	end
 
-	def set_value(x)
+	def setValue(x)
+		@number = x
+	end
+	
+	def increaseValueByX(x)
 		@number = @number + x
+	end
+
+	def increaseValueByOne()
+		@number = number + 1
+		return @number - 1
 	end
 end
 
 class B <A
 
-	def get_value()
-		return @number.to_s + " wow"
+	def getValue()
+		return @number.to_s + " B Class"
+	end
+
+	def increaseValueByX(x)
+		@number = @number +  3 + x
+	end
+
+	def setValue(x)
+		@number = x + 3
+	end
+
+	def decreaseValueByTwo()
+		@number = @number - 2
 	end
 end
 
-class C <B
+class C <B 
 
-	def initialize 
-		@number = 5
+	def initialize(number)
+		setValue(number + 5)
 	end
 	
-	def get_value()
-		return @number - 1
+	def toString()
+		return 'I am srtring number F ' + @number.to_s
+	end
+end
+
+class D <B
+	def initialize(number)
+		@number = number - 5 
+	end
+	
+	def d_value_is()
+		return @number
+	end
+
+	def increaseValueByOne()
+		@number = @number + 2
+		puts "oops it is incresed by two"
+	end
+end
+
+class E <C
+	def initialize()
+		@number = 5 
+	end
+	
+	def getValue()
+		return @number.to_s + " E Class"
 	end
 end
